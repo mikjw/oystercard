593b1bcac8e30a5874a52f429b710b6b6c24bc91
Mike <mjwalshe@gmail.com>
2019-08-27 16:05:00 +0100

    Balance limit tests passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
-
   attr_reader :balance
+
+  MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Top-up failed - maximum balance of #{MAXIMUM_BALANCE} exceeded" if (@balance + amount) > MAXIMUM_BALANCE 
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,7 +7,12 @@ describe Oystercard do
   end
 
   it 'tops up a card with the amount as argument' do
-    expect{subject.top_up(100)}.to change {subject.balance}.by(100)
+    expect{subject.top_up(89)}.to change {subject.balance}.by(89)
   end
 
+  it 'raises error when top-up would exceed maximum balance' do
+    max_bal = Oystercard::MAXIMUM_BALANCE
+    message = "Top-up failed - maximum balance of #{max_bal} exceeded"
+    expect { subject.top_up(max_bal + 1) }.to raise_error(message)
+  end 
 end
